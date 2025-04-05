e780e80192ed7a72210dd6f301cf9a4394e88392
Felipe Rosa <frbmoraisaero@gmail.com>
2025-04-05 15:30:42 -0300

    005_question_solution

---
+++ 005_Conditional_Statement_Question.c
@@ -0,0 +1,33 @@
+/* FONTE: HackerRank */
+
+/* Given a positive integer denoting , do the following:
+If , print the lowercase English word corresponding to the number (e.g., one for , two for , etc.).
+If , print Greater than 9. */
+
+/*
+Input 001
+
+5
+
+Expected Output 001
+
+five
+
+
+Input 002
+
+8
+
+Expected Output 002
+
+eight
+
+Input 003
+
+44
+
+Expected Output 003
+
+Greater than 9
+
+*/

aa06211ce35a389944a0e09dcc0b842cd11ef3d1
Felipe Rosa <frbmoraisaero@gmail.com>
2025-04-13 16:36:26 -0300

    030_question_solution

---
+++ 030_multiples_question.c
@@ -0,0 +1,27 @@
+/*
+FONTE: becrowd
+*/
+
+/*
+Read two integer values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+Input
+The input has two integer numbers.
+
+Output
+Print the relative message to the input as stated above.
+
+Input Sample	
+6 24
+
+Output Sample
+Sao Multiplos
+
+
+Input Sample
+6 25
+
+Output Sample
+Nao sao Multiplos
+
+*/

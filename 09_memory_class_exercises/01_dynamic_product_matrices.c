52fe42ff870d86a9bbb0fb1bb7db9732bdf955f3
Felipe Rosa <frbmoraisaero@gmail.com>
2025-05-04 21:53:22 -0300

    09_memory_class_exercises

---
+++ 09_memory_class_exercises/01_dynamic_product_matrices.c
@@ -0,0 +1,25 @@
+/*
+FONTE: 09-memory.pdf PES04 - Gustavo Carvalho
+*/
+
+
+/* QUESTION
+
+Write a program that takes two matrices from the standard input (with an arbitrary number
+of lines and columns), and computes the matrix product. If the matrices cannot be
+multiplied (i.e., the number of columns of the first matrix is different from the number of
+rows of the second matrix), print an error message, and terminate the program.
+
+*/
+
+/* SOLUTION
+Programmer: Felipe Rosa
+Date: 
+*/
+
+#include <stdio.h>
+
+
+int main() {
+    return 0;
+}
---
+++ 09_memory_class_exercises/02_pointers_arrays_structs.c
@@ -0,0 +1,26 @@
+/*
+FONTE: 09-memory.pdf PES04 - Gustavo Carvalho
+*/
+
+
+/* QUESTION
+
+Write a program that reads from the standard input a number of entries of type
+person_t. Ask the user the number of entries and, for each one, the number of
+characters of the associated name (modify the previous definition of person_t
+accordingly). Store the data in an array of pointers to person_t (i.e., person_t**).
+Finally, print all data in the standard output.
+
+*/
+
+/*
+Programmer: Felipe Rosa
+Date: 
+*/
+
+#include <stdio.h>
+
+
+int main() {
+    return 0;
+}
